04f0fde05df218fce4f1edd302337b045ee180c1
soysaucecrab <kimzinu07@naver.com>
2023-12-16 04:08:08 +0900

    1

---
+++ Assets/Script/PoolManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    //프리펩 보관 변수
+    public GameObject[] prefabs;
+    //풀 담당 리스트
+    List<GameObject>[] pools;
+
+}
