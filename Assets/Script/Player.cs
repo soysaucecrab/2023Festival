c2e5d80f9354aba556c2ca67cfb14a1db8d9c843
soysaucecrab <kimzinu07@naver.com>
2023-12-15 22:13:52 +0900

    1

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public Vector2 inputVec ;
+    public float speed;
+
+    SpriteRenderer spriter;
+
+    Rigidbody2D rigid;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        if (inputVec.x != 0 )
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
