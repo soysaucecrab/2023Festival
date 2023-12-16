6dca9c13912394cff4f4e6ab6adbb587615f1695
soysaucecrab <kimzinu07@naver.com>
2023-12-16 18:14:06 +0900

    church before

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -15,6 +15,34 @@ public class Weapon : MonoBehaviour
         Init();
     }
 
+    void Update()
+    {
+        switch (id)
+        {
+            case 0:
+                transform.Rotate(Vector3.back * speed * Time.deltaTime); //clock
+                break;
+
+            default: break;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            LevelUp(20, 5);
+        }
+    }
+
+    public void LevelUp(float damage, int count)
+    {
+        this.damage = damage;
+        this.count += count;
+
+        if(id == 0)
+        {
+            Batch();
+        }
+    }
+
     public void Init()
     {
         switch(id)
@@ -32,21 +60,29 @@ public class Weapon : MonoBehaviour
     {
         for(int i = 0; i < count; i++)
         {
-            Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
-            bullet.parent = transform;
+            Transform bullet;
+            if (i < transform.childCount)
+            {
+                bullet = transform.GetChild(i);
+            }
+            else
+            {
+                bullet = GameManager.instance.pool.Get(prefabId).transform;
+                bullet.parent = transform;
+            }
+            
+            
+
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity;
+
+            Vector3 rotVec = Vector3.forward * 360 * i / count;
+            bullet.Rotate(rotVec);
+            bullet.Translate(bullet.up * 1.1f, Space.World);
+
             bullet.GetComponent<Bullet>().Init(damage, -1); //infinity per ; sword
         }
     }
 
-    void Update()
-    {
-        switch (id)
-        {
-            case 0:
-                transform.Rotate(Vector3.back * speed * Time.deltaTime); //clock
-                break;
-
-            default: break;
-        }
-    }
+   
 }
